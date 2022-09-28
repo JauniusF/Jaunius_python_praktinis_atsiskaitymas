bae94bbf4713593dca1d989a4e5bea3c44022729
JauniusF <jaunius.filipoviciusx2@gmail.com>
2022-09-28 23:12:08 +0300

    va prie sito teko pasedet, per vs studio kazkodel man neveikia meta klaida jog neranda modulio, per cmd prompta veikia kuo puikiausiai

---
+++ Uzduotis_5/main.py
@@ -0,0 +1,18 @@
+
+from modules.mathematics.addition import add
+from modules.mathematics.division import divide
+from modules.mathematics.multiplication import multiply
+from modules.mathematics.subtraction import subtract
+import modules.num.ints as integers
+# Importuokite reikiamus modulius, kad veiktų žemiau nurodytos funkcijos:
+
+# Kitų failų ir žemiau esančio kodo nekeiskite
+a = add(integers.one, integers.four)
+b = divide(integers.four, integers.two)
+c = subtract(integers.three, integers.two)
+d = multiply(integers.five, integers.two)
+
+print(a)
+print(b)
+print(c)
+print(d)
